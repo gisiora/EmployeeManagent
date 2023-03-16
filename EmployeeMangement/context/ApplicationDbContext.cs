3d241b0685da2762b1c7003003f61ec74eaab3cf
gisiora <james.nyakundi@yahoo.com>
2023-03-16 05:57:58 +0300

    connection with DB

---
+++ EmployeeMangement/context/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using EmployeeMangement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeMangement.context
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>contextOptions)
+        :base(contextOptions)
+        {        
+             
+        }
+
+        //
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+
+
+
+
+
+    }
+}
